83c2815efe7a2204e1f582a8223835f10156230a
Rufat.Aliyev <rufataliyev1@outlook.fr>
2023-02-15 22:28:55 +0100

    Finished project by calling correct functions and sending transaction in the js file (have version incompatibility)

--- contracts/IUniswapV2Router01.sol
+++ contracts/IUniswapV2Router01.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.7;
 
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
--- contracts/IWeth.sol
+++ contracts/IWeth.sol
@@ -1,4 +1,4 @@
-
+//SPDX-License-Identifier: GPL-3.0
 // Copyright (C) 2015, 2016, 2017, 2019 Dapphub
 
 // This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.7;
 
 interface IWeth {
     function deposit() external payable;
